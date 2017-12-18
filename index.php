ffb3fa4703b4490e5dc8a4dc01bfe789500a08b4
phpdevdn <phpdevdn@users.noreply.github.com>
2017-12-18 14:10:35 +0700

    create index
    12-18 initial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Shop</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!--[if lt IE 9]>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+	<![endif]-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="" type="text/javascript"></script>
+	<style type="text/css" media="screen">
+		main{
+			min-height:calc(100vh - 100px);
+		}
+	</style>
+</head>
+<body>
+	<header id="header" class="">
+		<h1 class="text-center"><a href="/">lrv shop</a></h1>
+	</header>
+	<main>
+		<h2 class="text-center">Website is comming soon...</h2>	
+	</main>
+	<footer>
+		<p>Copyright&copy;2017</p>
+	</footer>
+</body>
+</html>
